6d95f9b1808ee9a333cdb1fafb133094a65d7983
Mario <m.leivaditis@gmail.com>
2017-10-16 23:46:46 +0300

    a

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const config = require("./config.json");
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
 
+var express = require('express')
+var app = express();
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
 app.listen(process.env.PORT || 3000 ,function(){
     console.log("up and running on port "+process.env.PORT);
 });
