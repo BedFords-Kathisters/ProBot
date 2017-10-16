9b7069ac8e9e3de2773b6f1302b09e227d9c823b
Mario <m.leivaditis@gmail.com>
2017-10-16 22:53:39 +0300

    removed async

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ client.on("guildDelete", guild => {
 });
 
 
-client.on("message", async message => {
+client.on("message", function(message){
   // This event will run on every single message received, from any channel or DM.
   
   // It's good practice to ignore other bots. This also makes your bot ignore itself
